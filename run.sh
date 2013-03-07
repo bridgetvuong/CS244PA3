0f9d7e5bbf56d0428e045b47b0a8ebc46a377bf9
Ubuntu <ubuntu@ip-10-252-32-147.us-west-2.compute.internal>
2013-03-07 01:13:34 +0000

    Write run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,19 @@
-sudo python pfabric.py --outputdir TCP --nhosts 54 --workload workloads/websearch.txt --nflows 100
+websearch = "workloads/websearch.txt"
+datamining = "workloads/datamining.txt"
+nflows = 100
+nhosts = 54
+cases = ("minTCP" "TCP")
+
+# Run on web search workload
+for i in "${cases[@]}"
+do
+sudo python pfabric.py --outputdir output_websearch --tcp $i --workload $websearch --nflows $nflows --nhosts $nhosts
+done
+
+# Run on data mining workload
+for i in "${cases[@]}"
+do
+sudo python pfabric.py --outputdir output_datamining --tcp $i --workload $datamining --nflows $nflows --nhosts $nhosts
+done
+
+# Plot
