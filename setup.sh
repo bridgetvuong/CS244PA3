d71c3056933061098a468c28bd529266259027c9
wmu <wendymu@gmail.com>
2013-03-11 20:03:59 -0700

    modify setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
+sudo rm -rf ~/mininet
+
 cd ~/CS244PA3/mininet
 sudo python setup.py install
 
 cd ~/CS244PA3
+
+sudo chmod a+x tcpconfig/TCPConfig.sh
+sudo chmod a+x tcpconfig/minTCPConfig.sh
+sudo chmod a+x run.sh
